562548cee90c861fc278ce73a1d39da56ae44808
mariannacoppa <densimetra@gmail.com>
2024-07-04 20:01:29 +0200

    [UPD] let user enter value and number

---
+++ PariDispari/js/script.js
@@ -0,0 +1,4 @@
+// permetto l'inserimento del valore e del numero da parte dell'utente
+let user_value = prompt('Inserisci la parola PARI o la parola DISPARI').toLowerCase();
+let user_num = parseInt(prompt('inserisci un numero compreso tra 1 e 5'));
+document.getElementById('user-num').innerText = user_num;
