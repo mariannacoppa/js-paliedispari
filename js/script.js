992f5144fd50c8989ec1475c0826596e92f12eb4
mariannacoppa <densimetra@gmail.com>
2024-07-04 15:51:54 +0200

    [UPD] given operation value

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // 1 dichiaro la funzione che deve verificare le una parola è palindroma
 function palDetect (word) {
-    
+    // 2 restituisco il valore dato dall'operazione:
+    // 2.1 prendo la parola inserita dall'utente, la divido in singole lettere, ne inverto l'ordine e riunisco le lettere
+    let check_pal = word.toLowerCase().split('').reverse().join('');
+    if (word === check_pal) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
