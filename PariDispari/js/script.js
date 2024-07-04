e8b11444f80efad4600fb8c79a324edcbd16ed7a
mariannacoppa <densimetra@gmail.com>
2024-07-04 20:02:56 +0200

    [UPD] set user number range and error alert

--- PariDispari/js/script.js
+++ PariDispari/js/script.js
@@ -2,3 +2,11 @@
 let user_value = prompt('Inserisci la parola PARI o la parola DISPARI').toLowerCase();
 let user_num = parseInt(prompt('inserisci un numero compreso tra 1 e 5'));
 document.getElementById('user-num').innerText = user_num;
+// obbligo l'utente ad inserire solo numeri compresi tra 1 e 5
+if (user_num > 0 && user_num <= 5) {
+    
+}
+// altrimenti compare messaggio di errore
+else {
+    alert('numero non è compreso tra 1 e 5');
+}
